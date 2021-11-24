4658a8b26e313bc875f17baaa4c36f0714808717
Jorge Gomez <jorge.gomez@thoughtworks.com>
2021-11-24 12:19:36 +0100

    Shower thought

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,11 +20,8 @@ const Home: NextPage<HomeProps> = ({posts}) => {
           <title>{siteTitle}</title>
         </Head>
         <section className={utilStyles.headingMd}>
-          <p>Holiis!</p>
-          <p>
-            (This is a sample website - you’ll be building a site like this on{' '}
-            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-          </p>
+          <p>La página de Jorge</p>
+          <p></p>
         </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
           <h2 className={utilStyles.headingLg}>Blog</h2>
---
+++ posts/past-self.md
@@ -0,0 +1,4 @@
+---
+title: 'Your past self is both old you and young you'
+date: '2021-11-24'
+---

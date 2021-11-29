1fd84bbcb6a432aeed251f1e1a36c5f28ee3ab91
Jorge Gomez <jorge.gomez@thoughtworks.com>
2021-11-29 11:16:19 +0100

    Pushing to git

--- createPost.ts
+++ createPost.ts
@@ -1,10 +1,12 @@
 import yargs from "yargs/yargs";
 import matter from "gray-matter";
 import {format} from "date-fns";
+import simpleGit from "simple-git";
 import path from "path";
 import fs from "fs";
 
 const postsDirectory = path.join(process.cwd(), 'posts');
+const git = simpleGit();
 
 function parseArguments() {
     return Promise.resolve(yargs(process.argv.slice(2)).options({
@@ -13,10 +15,22 @@ function parseArguments() {
     }).argv);
 }
 
-parseArguments().then(({id, title}) => {
-    const fullPath = path.join(postsDirectory, `${id}.md`);
+function writeFile(filePath: string, title: string) {
     const content = matter.stringify('', {title, date: format(new Date(), 'yyyy-MM-dd')});
-    fs.writeFileSync(fullPath, content, 'utf8');
-});
+    fs.writeFileSync(filePath, content, 'utf8');
+}
+
+async function main() {
+    const {id, title} = await parseArguments();
+    const filePath = path.join(postsDirectory, `${id}.md`);
+    writeFile(filePath, title);
+    await git.pull();
+    await git.add(filePath);
+    await git.commit(id);
+    await git.push();
+}
+
+main().catch(console.error);
+
 
 
